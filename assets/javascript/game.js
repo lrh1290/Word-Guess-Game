fbe8ac07742a2def15e2f1c5c34108b058306368
lrh1290 <38796653+lrh1290@users.noreply.github.com>
2018-07-27 23:27:08 -0500

    Add files via upload

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,10 @@
 var currentword = document.getElementById("currentword");
+var previousword = document.getElementById("previousword");
 var wins = document.getElementById("wins");
 var losses = document.getElementById("losses");
 var guessCounter = document.getElementById("guess-counter");
 var guessed = document.getElementById("guessed");
-
+var gunslinger = document.getElementById("gunslinger");
 
 var game = {
     wordlist: ["BillytheKid","BuffaloBill","ButchCassidy","JohnWesleyHardin","NatLove","WilliamBrocius","TexasJack","TomKetchum","TomHorn","HarveyLogan","SamBass"],
@@ -12,11 +13,12 @@ var game = {
     lives: [],
     guessed: [],
     secretWord: "",
+    previousword: "",
     guessCounter: 10,
     evaluateGuess: function(guess) {
+        var match = false;
         var lowercaseGuess = guess.toLowerCase();
         var lowercaseSecretWord = this.secretWord.toLowerCase().split("");
-        var match = false;
         for (var i = 0; i < lowercaseSecretWord.length; i++) {
             if (lowercaseSecretWord[i] === lowercaseGuess) {
                 match = true;
@@ -34,26 +36,34 @@ var game = {
         }
     },
     setNewSecretWord: function() {
-        this.resetLives();
+        this.giveLives();
         this.secretWord = this.wordlist[Math.floor(Math.random() * Math.floor(this.wordlist.length))];
     },
-    resetLives: function() {
+    setGunSlinger: function() {
+        gunslinger.removeAttribute("class");
+    },
+    setPrevSecretWord: function() {
+        this.previousword = this.secretWord;
+    },
+    giveLives: function() {
         var player = this;
         this.lives.forEach(function(letter, index) {
             delete player.lives[index];
         });
     },
-    render: function(str) {
+    rendergame: function(str) {
         currentword.textContent = this.guessed.join("");
         guessCounter.textContent = this.guessCounter;
         lives.textContent = this.lives.join(" ");
         wins.textContent = this.wins;
         losses.textContent = this.losses;
+        previousword.textContent = this.previousword;
+        gunslinger.innerHTML = '<img src="assets/images/'+ this.secretWord + '.jpg" class="img-fluid">';
     }
 };
 game.setNewSecretWord();
 game.setGuessed();
-game.render();
+game.rendergame();
 document.onkeyup = function(event) {
     var userGuess = event.key;
     var guessed = game.guessed.join("");
@@ -63,25 +73,29 @@ document.onkeyup = function(event) {
         if (correct) {
             game.evaluateGuess(userGuess);
             game.wins++;
+            game.setPrevSecretWord();
             game.guessCounter = 10;
             game.guessed = [];
             game.secretWord = "";
             game.setNewSecretWord();
             game.setGuessed();
+            game.setGunSlinger();
         } else if (game.guessCounter > 0 && guessed !== game.secretWord) {          
             var match = game.evaluateGuess(userGuess);
             if (!match) {
                 game.guessCounter--;
                 if (game.guessCounter === 0) {
                     game.losses++;
+                    game.setPrevSecretWord();
                     game.guessCounter = 10;
                     game.guessed = [];
                     game.secretWord = "";
                     game.setNewSecretWord();
                     game.setGuessed();
+                    game.setGunSlinger();
                 }
             }
         } 
     }
-    game.render();
+    game.rendergame();
 };
